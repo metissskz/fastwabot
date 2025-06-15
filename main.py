2a346aa9281e2bd1fa8ade6bc908418e6047f1a8
metissskz <132703265+metissskz@users.noreply.github.com>
2025-06-16 01:22:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 
-import asyncio
-import logging
 import os
-
-from aiogram import Bot, Dispatcher, types
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.types import Message
+from aiogram.filters import Command
 
-# Загружаем токен из переменной окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Проверка токена
-if not BOT_TOKEN:
-    raise ValueError("Переменная окружения BOT_TOKEN не установлена")
-
-# Создаем экземпляр бота и диспетчера
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
-@dp.message(commands=["start"])
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Салам, <b>{message.from_user.full_name}</b>!")
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer("Привет! Я запущен и готов к работе.")
 
-async def main() -> None:
-    logging.basicConfig(level=logging.INFO)
+async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
